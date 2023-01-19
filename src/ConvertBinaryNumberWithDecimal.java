0bd1a48f33973fb33280a6a1fa6afa72d884f473
Narek Poghosyan <narekpoxosyan95@gmail.com>
2023-01-19 14:01:57 +0400

    Add task about prime numbers

--- src/ConvertBinaryNumberWithDecimal.java
+++ src/ConvertBinaryNumberWithDecimal.java
@@ -1,21 +1,10 @@
 public class ConvertBinaryNumberWithDecimal {
     public static void main(String[] args) {
-        int binaryArray[] = {0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1};
+        int binaryArray[] = {0, 0, 1,};
         int decimal = 0;
-        int index = 0;
         for (int i = 0; i < binaryArray.length; i++) {
-            if (binaryArray[i] == 1) {
-                if (i == binaryArray.length - 1 && binaryArray[i] == 1) {
-                    decimal += 1;
-                } else {
-                    int binary = 1;
-                    for (int j = 0; j < binaryArray.length - 1 - i; j++) {
-                        binary *= 2;
-                    }
-                    decimal += binary;
-                }
-            }
+            decimal = decimal * 2 +binaryArray[i];
         }
-        System.out.println(decimal);
+        System.out.println("The corresponding decimal number is " + decimal);
     }
 }
---
+++ src/PrimeNumbers.java
@@ -0,0 +1,20 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int array[] = {6, 1, 4, 8, 7, 9, 12};
+        boolean cond = false;
+        for (int i = 0; i < array.length; i++) {
+            int num = 0;
+            for (int j = 1; j <= array[i] / 2; j++) {
+                if (array[i] % j == 0)
+                    num++;
+            }
+            if (num == 1) {
+                cond = true;
+                System.out.println(array[i] + " is prime");
+            }
+        }
+        if (!cond) {
+            System.out.println("No prime number found in this array");
+        }
+    }
+}
