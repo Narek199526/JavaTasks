26b3d30fd3c8dd4c0d35d69584e8526a2c06c400
Narek Poghosyan <narekpoxosyan95@gmail.com>
2023-01-30 23:20:20 +0400

    Add corect task from method

--- src/methods/SquareByMethod.java
+++ src/methods/SquareByMethod.java
@@ -2,25 +2,31 @@ package methods;
 
 public class SquareByMethod {
     public static void main(String[] args) {
-        System.out.println(sqr(2, -1));
-    }
-
-    static double sqr(int a, int b) {
-        double c = 1;
-        for (int i = 0; i < Math.abs(b); i++) {
-            c *= a;
+        double sqr1 = sqr(-2, -1);
+       // System.out.print(a + "'s " + " degree " + b + " is ");
+       // System.out.print("You need to correct your inputs and exit cod is ");
+        if (sqr1 == -1){
+            System.out.print("You need to correct your inputs ");
+        }else if (sqr1 == -2){
+            System.out.println("'a' can't be negative ");
+        }else {
+            System.out.print("The degree is " + sqr1);
         }
+    }
+    static double sqr( int a, int b) {
+        double c = 1;
+
         if (Math.abs(a) > 10 || Math.abs(b) > 10) {
-            System.out.print("You need to correct your inputs and exit cod is ");
             return -1;
-        }else if (a > 0 && b > 0)
-            System.out.print(a + "'s " + " degree " + b + " is ");
-        else if (a < 0) {
-            System.out.print("You need to correct your inputs and exit cod is ");
+        } else if (a > 0 && Math.abs(b) > 0) {
+            for (int i = 0; i < Math.abs(b); i++) {
+                c *= a;
+            }
+            if (b < 0){
+                c =  1 / c;
+            }
+        } else if (a < 0) {
             return -2;
-        } else if (b < 0) {
-            System.out.print(a + "'s " + " degree " + b + " is ");
-            return ((double)1 / c);
         }
         return c;
     }
