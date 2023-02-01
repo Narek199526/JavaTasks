412af03a13bbce43e285856cf5953cbcdeeab1b2
Narek Poghosyan <narekpoxosyan95@gmail.com>
2023-02-01 17:59:46 +0400

    Add the final version of the task ArmstrongNuber

--- src/methods/ArmstrongNumber.java
+++ src/methods/ArmstrongNumber.java
@@ -2,17 +2,17 @@ package methods;
 
 public class ArmstrongNumber {
     public static void main(String[] args) {
-        int num = 153;
-        int numberDegreeSum = numberArmstrong(num);
-        if (num == numberDegreeSum){
-            System.out.println(num + " is armstrong");
-        } else System.out.println(num + " isn't armstrong");
-        }
-    static  int numberArmstrong(int num){
+        int num = 158;
+        if (numberArmstrong(num))System.out.println(num + " is armstrong");
+         else System.out.println(num + " isn't armstrong");
+    }
+
+    static boolean numberArmstrong(int num) {
+        int initialNumber = num;
         int numberDegree = 1;
         int numberDegreeSum = 0;
         int numberLength = numberLength(num);
-        while (num > 0){
+        while (num > 0) {
             numberDegree = num % 10;
             for (int i = 1; i < numberLength; i++) {
                 numberDegree = numberDegree * (num % 10);
@@ -20,15 +20,17 @@ public class ArmstrongNumber {
             numberDegreeSum += numberDegree;
             num /= 10;
         }
-        return numberDegreeSum;
+        if (initialNumber == numberDegreeSum) {
+            return true;
+        } else return false;
     }
 
-    static int numberLength(int number){
+    static int numberLength(int number) {
         int count = 0;
-       while (number > 0){
-           number = number / 10;
-           count++;
-       }
-       return count;
+        while (number > 0) {
+            number = number / 10;
+            count++;
+        }
+        return count;
     }
 }
