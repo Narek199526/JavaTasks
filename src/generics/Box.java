10d29b4aae5a371b1fbb0a3cee635fad870ff351
Narek Poghosyan <narekpoxosyan95@gmail.com>
2023-05-23 12:47:05 +0400

    add Box task

--- src/generics/Box.java
+++ src/generics/Box.java
@@ -13,30 +13,38 @@ public class Box<T extends Number> {
     }
 
     public void setLength(T length) {
-        this.length = length;
+        if (length.doubleValue() > 0) {
+            this.length = length;
+        }
     }
     public T getLength(){
         return length;
     }
 
     public void setHeight(T height) {
-        this.height = height;
+        if (height.doubleValue() > 0) {
+            this.height = height;
+        }
     }
     public T getHeight() {
         return height;
     }
 
     public void setWidth(T width) {
-        this.width = width;
+        if (width.doubleValue() > 0){
+            this.width = width;
+        }
     }
     public T getWidth() {
         return width;
     }
 
 
-
     public static Number sum(Box<?> box, Box<?> box1){
         return box.length.doubleValue() + box.width.doubleValue() + box.height.doubleValue() + box1.length.doubleValue() + box1.width.doubleValue() + box1.height.doubleValue() ;
     }
 
+//    public static Number sum(Box box, Box box1){
+//        return box.length.doubleValue() + box.width.doubleValue() + box.height.doubleValue() + box1.length.doubleValue() + box1.width.doubleValue() + box1.height.doubleValue() ;
+//    }
 }
