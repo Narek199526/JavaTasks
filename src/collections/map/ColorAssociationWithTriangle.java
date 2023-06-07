14dda36f5cdc7434b57eb805085a84f4345ce391
Narek Poghosyan <narekpoxosyan95@gmail.com>
2023-06-07 10:07:13 +0400

    Add Maps tasks final version

--- src/collections/map/ColorAssociationWithTriangle.java
+++ src/collections/map/ColorAssociationWithTriangle.java
@@ -3,32 +3,34 @@ package collections.map;
 import java.util.LinkedHashMap;
 
 public class ColorAssociationWithTriangle {
-    static boolean comareTo(Object t,String color ){
-        if (t.equals(color)){
-            return true;
-        } else
-            return false;
-    }
-
-
     public static void main(String[] args) {
-        LinkedHashMap<Triangle , String > linkedHashMap = new LinkedHashMap<>();
-        Triangle blackTriangle = new Triangle( 5, 1, 2, 3, 5, 9);
-        Triangle redTriangle = new Triangle(3, 5, 6, 9, 4, 2);
-        Triangle yellowTriangle = new Triangle(1, 6, 9, 4,3, 6);
-        Triangle greenTriangle = new Triangle(4, 9, 7, 8, 5, 4);
-        Triangle secondBlackTriangle = new Triangle(5, 6, 8, 5, 1, 80);
+        int index = 0;
 
-        linkedHashMap.put(blackTriangle, "Black");
-        linkedHashMap.put(redTriangle, "Red");
-        linkedHashMap.put(yellowTriangle, "Yellow");
-        linkedHashMap.put(greenTriangle, "Green");
-        linkedHashMap.put(secondBlackTriangle, "Black");
-        System.out.println(linkedHashMap.get(blackTriangle));
-        System.out.println(linkedHashMap.get(secondBlackTriangle));
-        System.out.println(linkedHashMap.get(redTriangle));
-        System.out.println(linkedHashMap.get(yellowTriangle));
-        System.out.println(linkedHashMap.get(greenTriangle));
+        LinkedHashMap<Triangle, String> linkedHashMap = new LinkedHashMap<>();
+
+        Triangle[] triangles = {
+                new Triangle(5, 1, 2, 3, 5, 9),
+                new Triangle(3, 5, 6, 9, 4, 2),
+                new Triangle(1, 6, 9, 4, 3, 6),
+                new Triangle(4, 9, 7, 8, 5, 4),
+                new Triangle(5, 6, 8, 5, 1, 80)
+        };
+
+
+        String[] strings = {"Black", "Red", "Yellow", "Green", "Black"};
+        for (int i = 0; i < triangles.length; i++) {
+            linkedHashMap.put(triangles[i], strings[i]);
+        }
+
+
+        for (Triangle t : linkedHashMap.keySet()) {
+            if (linkedHashMap.get(t).equals(strings[index])) {
+                System.out.println("Pass");
+            } else {
+                System.out.println("Fail");
+            }
+            index++;
+        }
 
     }
 
--- src/collections/map/YearOfIssueAssociationWithPixel.java
+++ src/collections/map/YearOfIssueAssociationWithPixel.java
@@ -6,29 +6,27 @@ import java.util.TreeMap;
 
 public class YearOfIssueAssociationWithPixel {
     public static void main(String[] args) {
-        Monitor monitor1 = new Monitor(120, 5000);
-        Monitor monitor2 = new Monitor(100, 4500);
-        Monitor monitor3 = new Monitor(90, 2000);
-        Monitor monitor4 = new Monitor(150, 3000);
-        Monitor monitor5 = new Monitor(160, 3500);
-        Monitor monitor6 = new Monitor(200, 2500);
+        Monitor[] monitors = {
+                new Monitor(120, 5000),
+                new Monitor(100, 4500),
+                new Monitor(90, 2000),
+                new Monitor(150, 3000),
+                new Monitor(160, 3500),
+                new Monitor(200, 2500)
+        };
+
+        String [] strings = {"Samsung 2018", "Hisense 2019", "LG 2018", "Philips 2020", "Sony 2020", "Panasonic 2022"};
+
         TreeMap<Monitor, String> treeMap = new TreeMap<>();
-        treeMap.put(monitor1, "Samsung 2018");
-        treeMap.put(monitor2, "Hisense 2019");
-        treeMap.put(monitor3, "LG 2018");
-        treeMap.put(monitor4, "Philips 2020");
-        treeMap.put(monitor5, "Sony 2020");
-        treeMap.put(monitor6, "Panasonic 2022");
 
-
-//        Set<Map.Entry<String, Monitor>> entries = treeMap.entrySet();
-//        entries.forEach(stringMonitorEntry -> {
-//            System.out.println(stringMonitorEntry.getKey() + "->" +stringMonitorEntry.getValue() );
-//        });
-        for (Monitor key:treeMap.keySet()) {
-            System.out.println(treeMap.get(key) + ", diagonal-" + key.getDiagonal() + " pixel-" + key.getPixel());
+        for (int i = 0; i < monitors.length; i++) {
+            treeMap.put(monitors[i], strings[i]);
         }
 
+
+        for (Monitor key : treeMap.keySet()) {
+            System.out.println(treeMap.get(key) + ", diagonal-" + key.getDiagonal() + " pixel-" + key.getPixel());
+        }
 
     }
 
